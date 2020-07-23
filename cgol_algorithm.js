312b9c63d929558f6a52314033b03a727477bbc2
lesley <lesleybanadzem2017@gmail.com>
2020-07-23 11:49:39 -0700

    Wrote conditional to get the northWest element of a current cell

--- cgol_algorithm.js
+++ cgol_algorithm.js
@@ -16,9 +16,23 @@ for (let i = 0; i < numRows; i++) {
 			let west = rows[i][k - 1] ? rows[i][k - 1] : rows[i][9]; // If the left element is undefine get the right most element
 			let east = rows[i][k + 1] ? rows[i][k + 1] : rows[i][0];
 
-			console.log(east);
+			let northWest;
+			if (rows[i - 1] === undefined && k === 0) {
+				// If we are at first element of the top row the the last element of the bottom row
+				northWest = rows[numRows - 1][9];
+			} else if (rows[i - 1] === undefined && k > 0) {
+				// IF we are at the top row and the second column, get the element to the left of the bottom column
+				northWest = rows[numRows - 1][k - 1];
+			} else {
+				// Else get the element of the bottom
+				northWest = rows[i - 1][k - 1];
+			}
+			console.log("hello", i, k);
+			console.log(northWest);
 			console.table(rows);
-			break;
+			if (k === 9) {
+				break;
+			}
 		}
 		break;
 	}
