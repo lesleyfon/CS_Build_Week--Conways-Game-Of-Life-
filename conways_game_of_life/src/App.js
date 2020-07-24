650bdd7c124ceaf97af27f2ca3f624f7364e1633
lesley <lesleybanadzem2017@gmail.com>
2020-07-24 11:06:38 -0700

    Added some minor styles to the grid

--- conways_game_of_life/src/App.js
+++ conways_game_of_life/src/App.js
@@ -16,26 +16,27 @@ function App() {
 		return rows;
 	});
 
-	console.log(grid);
 	return (
 		<main className="App">
 			<section
 				className="game_grid"
 				style={{
 					display: "grid",
-					gridTemplateColumns: `repeat(${rowsCols.cols}, 20px)`,
+					gridTemplateColumns: `repeat(${rowsCols.cols}, 50px)`,
 					margin: "0 auto",
 				}}
 			>
 				{grid.map((row, i) =>
-					row.map((cel) => (
+					row.map((cel, k) => (
 						<div
+							key={`${i}-${k}`}
+							onClick={() => console.log(i, k)}
 							style={{
 								border: "1px solid black",
+								backgroundColor: cel === 1 ? "#DFBBF2" : null,
+								height: "50px",
 							}}
-						>
-							{cel}
-						</div>
+						/>
 					))
 				)}
 			</section>
