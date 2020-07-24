60ddf75bdd9aba52fb026ead4cde1342ec8643c2
lesley <lesleybanadzem2017@gmail.com>
2020-07-24 10:37:19 -0700

    Created state for the grid

--- conways_game_of_life/src/App.js
+++ conways_game_of_life/src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+	const [rowsCols, setRowsCols] = useState({
+		rows: 10,
+		cols: 10,
+	});
+
+	const [grid, setGrid] = useState(() => {
+		let rows = [];
+		for (let i = 0; i < rowsCols.rows; i++) {
+			rows.push(Array.from(Array(rowsCols.cols), () => (Math.random() > 0.6 ? 1 : 0)));
+		}
+		return rows;
+	});
+
+	console.log(grid);
 	return <div className="App">Hello</div>;
 }
 
