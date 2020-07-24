6c985279f6b1c7116442ac7d5e39deff01e1b0c8
lesley <lesleybanadzem2017@gmail.com>
2020-07-24 10:51:15 -0700

    displaying grid on the screen with Zeroes and Ones

--- conways_game_of_life/src/App.js
+++ conways_game_of_life/src/App.js
@@ -9,6 +9,7 @@ function App() {
 
 	const [grid, setGrid] = useState(() => {
 		let rows = [];
+
 		for (let i = 0; i < rowsCols.rows; i++) {
 			rows.push(Array.from(Array(rowsCols.cols), () => (Math.random() > 0.6 ? 1 : 0)));
 		}
@@ -16,7 +17,30 @@ function App() {
 	});
 
 	console.log(grid);
-	return <div className="App">Hello</div>;
+	return (
+		<main className="App">
+			<section
+				className="game_grid"
+				style={{
+					display: "grid",
+					gridTemplateColumns: `repeat(${rowsCols.cols}, 20px)`,
+					margin: "0 auto",
+				}}
+			>
+				{grid.map((row, i) =>
+					row.map((cel) => (
+						<div
+							style={{
+								border: "1px solid black",
+							}}
+						>
+							{cel}
+						</div>
+					))
+				)}
+			</section>
+		</main>
+	);
 }
 
 export default App;
